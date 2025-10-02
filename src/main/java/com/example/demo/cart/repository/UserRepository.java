f5fb28462dece476ec6ce87ecfe9195d648f0e40
Trick22618 <pofgh986521@yahoo.com.tw>
2025-10-02 18:05:38 +0800

    update

---
+++ src/main/java/com/example/demo/cart/repository/UserRepository.java
@@ -0,0 +1,24 @@
+package com.example.demo.cart.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.cart.model.entity.User;
+import java.util.List;
+import java.util.Optional;
+
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>{
+
+	List<User> findByUsername(String username); // 屬於多筆查詢
+	
+	Optional<User> findFirstByUsername(String username); // 屬於單筆查詢
+	
+	// 利用原生 sql
+	@Query(value =  "select * from user where username = :username", nativeQuery = true)
+	Optional<User> findByUsernameNative(@Param("username") String name);
+	
+}
---
+++ src/test/java/com/example/demo/test/FindUser.java
@@ -0,0 +1,44 @@
+package com.example.demo.test;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.demo.cart.model.entity.User;
+import com.example.demo.cart.repository.UserRepository;
+
+@SpringBootTest
+public class FindUser {
+
+	@Autowired
+	UserRepository userRepository;
+	
+	@Test
+	void find() {
+		// 取得單筆資料
+		//Optional<User> optUser = userRepository.findById(1L);
+		//Optional<User> optUser = userRepository.findFirstByUsername("mary");
+		Optional<User> optUser = userRepository.findByUsernameNative("mary");
+		if(optUser.isEmpty()) {
+			System.out.println("查無資料");
+		} else {
+			User user = optUser.get();
+			System.out.printf("%d\t%s\t%s%n", user.getId(), user.getUsername(), user.getPassword());
+ 		}
+		
+		// 取得多筆
+		//List<User> users = userRepository.findAll();		
+		// 透過自訂方法
+		List<User> users = userRepository.findByUsername("john");
+		
+		System.out.printf("資料筆數: %d%n", users.size());
+		users.forEach(user -> {
+			System.out.printf("%d\t%s\t%s%n", user.getId(), user.getUsername(), user.getPassword());			
+		});
+
+	}
+	
+}
